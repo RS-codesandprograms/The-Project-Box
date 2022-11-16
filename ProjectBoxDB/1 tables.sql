3e19a7b544eb496b8a47fef4a15fe5b8d27468e1
AvigailGellis <agellis@cpucodeschool.com>
2022-11-16 10:21:58 +0200

    #2

--- ProjectBoxDB/1 tables.sql
+++ ProjectBoxDB/1 tables.sql
@@ -7,10 +7,18 @@ go
 create table dbo.ProjectBox(
     ProjectBoxId int not null identity primary key,
     OrganizationName varchar(50) not null constraint ck_ProjectBox_OrganizationName_cannot_be_blank check(OrganizationName <> ''),
-    ProjectType varchar(100) not null constraint ck_ProjectBox_ProjectType_must_be_one_of_the_selling_choices check(ProjectType in('Candy pillow kit', 'Felt doll kit', 'Throw blanket kit', 'Felt travel game kit', 'Stuffed initial kit')),
+    ProjectType varchar(20) not null constraint ck_ProjectBox_ProjectType_must_be_one_of_the_selling_choices check(ProjectType in('Candy pillow kit', 'Felt doll kit', 'Throw blanket kit', 'Felt travel game kit', 'Stuffed initial kit')),
     OrderAmount int not null constraint ck_ProjectBox_OrderAmount_must_be_between_15_and_1000 check(OrderAmount between 15 and 1000),
-    OrderDate datetime not null constraint ck_ProjectBox_OrderDate_must_be_between_february_5_2010_and_the_current_date check(OrderDate between '2010-02-05' and getdate()),
-    DeliveryDate datetime constraint ck_ProjectBox_DeliveryDate_must_be_between_february_5_2010_and_the_current_date check(DeliveryDate between '2010-02-05' and getdate()),
+--AS No reason, many times we just use datetime.
+    OrderDate date not null constraint ck_ProjectBox_OrderDate_must_be_between_february_5_2010_and_the_current_date check(OrderDate between '2010-02-05' and getdate()),
+    DeliveryDate date constraint ck_ProjectBox_DeliveryDate_must_be_between_february_5_2010_and_the_current_date check(DeliveryDate between '2010-02-05' and getdate()),
+--AS You can't make a computed column based on 2 values, you have to give it one value, that is why this wasn't an option. I left commented out code below just in case you want to keep how it was done before.
+    EstimatedDeliveryDate as case 
+                                when OrderAmount <= 50 then dateadd(day, 7, OrderDate)
+                                when OrderAmount between 51 and 200 then dateadd(day, 14, OrderDate)
+                                when OrderAmount between 201 and 350 then dateadd(day, 21, OrderDate)
+                                when OrderAmount > 350 then dateadd(day, 31, OrderDate)
+                             end,
     Paid bit not null default 1,
 --AS The data you gave that each project type has a different price and each amount has a different calculation would 
 --AS involve writing 20 case statements, that really isn't to fit into one table. I changed it that all projects cost $5/piece.
@@ -20,13 +28,16 @@ create table dbo.ProjectBox(
                      when OrderAmount between 201 and 350 then (5 - 1.5) * OrderAmount
                      when OrderAmount > 350 then (5 - 2.25) * OrderAmount
                   end,
-    ReferredBy varchar(50) not null,
-    Survey varchar(50) not null constraint ck_ProjectBox_Survey_must_be_one_of_the_survey_options check(Survey in('extremely unsatisfied',  'partially satisfied', 'satisfied', 'very satisfied', 'extremely satisfied', 'no rating', 'not rated yet')),
-    constraint ck_ProjectBox_DeliveryDate_is_correct_estimated_date 
-        check((OrderAmount <= 50 and DeliveryDate = dateadd(day, 7, OrderDate))      
-           or ((OrderAmount between 51 and 200) and (DeliveryDate between dateadd(day, 7, OrderDate) and dateadd(day, 14, OrderDate)))
-           or ((OrderAmount between 201 and 350) and (DeliveryDate between dateadd(day, 14, OrderDate) and dateadd(day, 21, OrderDate)))
-           or (OrderAmount > 350 and (DeliveryDate between dateadd(day, 21, OrderDate) and dateadd(day, 31, OrderDate)))
-        )          
+    ReferredBy varchar(13) not null constraint ck_ProjectBox_ReferredBy_must_be_one_of_the_options check(ReferredBy in('Phonebook', 'Magazine ad', 'Online', 'Word of mouth', 'Other')),
+    Survey varchar(21) null constraint ck_ProjectBox_Survey_must_be_one_of_the_survey_options check(Survey in('extremely unsatisfied',  'partially satisfied', 'satisfied', 'very satisfied', 'extremely satisfied', 'no rating')),
+--AS Yes I didn't add in this constraint before because of the constraint below.
+    constraint ck_ProjectBox_OrderDate_must_be_before_DeliveryDate check(OrderDate < DeliveryDate),
+    constraint ck_ProjectBox_Survey_blank_if_item_not_delivered_yet check((DeliveryDate is null and Survey is null) or (DeliveryDate is not null and Survey is not null))
+   --constraint ck_ProjectBox_DeliveryDate_is_correct_estimated_date 
+   --    check((OrderAmount <= 50 and DeliveryDate = dateadd(day, 7, OrderDate))      
+   --       or ((OrderAmount between 51 and 200) and (DeliveryDate between dateadd(day, 7, OrderDate) and dateadd(day, 14, OrderDate)))
+   --       or ((OrderAmount between 201 and 350) and (DeliveryDate between dateadd(day, 14, OrderDate) and dateadd(day, 21, OrderDate)))
+   --       or (OrderAmount > 350 and (DeliveryDate between dateadd(day, 21, OrderDate) and dateadd(day, 31, OrderDate)))
+   --    )          
 )
 go
--- ProjectBoxDB/3 reports.sql
+++ ProjectBoxDB/3 reports.sql
@@ -13,9 +13,12 @@ select top 1 LowestMarketing = count(*), p.ReferredBy
 from ProjectBox p 
 group by p.ReferredBy
 order by LowestMarketing
---3.	Please show a list of orders where the delivery time was longer than promised to the customer. This list should be ordered by order size, from least to greatest. This is something I always need to know.
---AS This question cannot be done because in order to follow the guidelines for delivery date that you gave, I had to add a constraint to make sure all delivery dates are correct.
---AS Therefore all dates in the data now are exactly according to the guidelines.
+--3.	Please show a list of orders where the delivery time was longer than promised to the customer. 
+--This list should be ordered by order size, from least to greatest. This is something I always need to know.
+select p.OrganizationName, ExtraTimeTookToDeliver = datepart(day,p.DeliveryDate) - datepart(day,p.EstimatedDeliveryDate)
+from ProjectBox p 
+where p.DeliveryDate > p.EstimatedDeliveryDate
+order by p.OrderAmount
 --4.	Please show how many of each rating we received. I also need a list of orders that were rated below satisfied. 
 select NumSurvey = count(*), p.Survey
 from ProjectBox p 
